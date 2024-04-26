d917e78b1379f1408963f7e22c824286cd70c862
francescofalcone05 <ciccio_97@live.it>
2024-04-26 14:36:36 +0300

    first commit

---
+++ js/main1.js
@@ -0,0 +1,32 @@
+
+//NEL README TROVO L'ORDINE DEGLI SNACK
+
+/***************SNACK NR.1****************/
+
+// //inizializzo il numero che poi verra dato dallo user
+// let userNumber= ''
+
+// //finche` userNumber e` diverso da 42
+// while (userNumber != 42) {
+    
+//     //chiedi tramite prompt di inserire un nmero 
+//     userNumber = prompt ('Inserisci il tuo numero per sapere se hai vinto')
+
+// }
+
+// //se il numero inserito e` uguale a 42 
+// if (userNumber == 42){
+
+//     //stampa un'alert
+//     alert ('Congratulazioni, hai indovinato il numero vincente')
+//     }
+
+
+/***************SNACK NR.2****************/
+
+
+
+
+
+    
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+SNACK
+
+1) Chiedi un numero all'utente tramite prompt. Finchè il numero inserito non è 42, continua a chiedere il numero. Se il numero è 42 congratulati.
+
