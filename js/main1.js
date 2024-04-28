c7968d6cec9d1483cb7bb2b7b1154a85a923a06d
francescofalcone05 <ciccio_97@live.it>
2024-04-28 16:16:38 +0300

    snack-nr3

--- js/main1.js
+++ js/main1.js
@@ -24,30 +24,51 @@
 
 /***************SNACK NR.2****************/
 
-//inizializzo le variabili vuote che poi andro` a definire
-let listaNumeri = ['']
-let userNumber = ''
-let somma = parseInt(0)
+// //inizializzo le variabili vuote che poi andro` a definire
+// let listaNumeri = ['']
+// let userNumber = ''
+// let somma = parseInt(0)
 
 
- for (let i = 1; i <= 10; i++){
+//  for (let i = 1; i <= 10; i++){
 
-    //chiedo allo user di inserire un numero, il quale viene convertito in numero dal parseInt()
-    userNumber = parseInt(prompt('Inserisci un numero'))
+//     //chiedo allo user di inserire un numero, il quale viene convertito in numero dal parseInt()
+//     userNumber = parseInt(prompt('Inserisci un numero'))
     
     
-    //aggiungo i vari numeri all`array
-    listaNumeri.push(userNumber)
+//     //aggiungo i vari numeri all`array
+//     listaNumeri.push(userNumber)
     
 
-    //sommo tutti i numeri
-    //somma = listaNumeri[1] + listaNumeri[2] + listaNumeri[3] + listaNumeri[4] + listaNumeri[4] + listaNumeri[6] + listaNumeri[7] + listaNumeri[8] + listaNumeri[9] + listaNumeri[10]
-    somma += listaNumeri[i]
+//     //sommo tutti i numeri
+//     //somma = listaNumeri[1] + listaNumeri[2] + listaNumeri[3] + listaNumeri[4] + listaNumeri[4] + listaNumeri[6] + listaNumeri[7] + listaNumeri[8] + listaNumeri[9] + listaNumeri[10]
+//     somma += listaNumeri[i]
     
 
-}
+// }
 
-//stampo il totale
-alert(somma)
+// //stampo il totale
+// alert(somma)
+
+
+/***************SNACK NR.3****************/
+
+//chiedo le due parole tramite i prompt
+//let firstWord = prompt('inserisci qui la parola nr. 1')
+//let secondWord = prompt('inserisci qui la parola nr. 2')
+//let longest = ''
+
+////a seconda di quale length è più grande stabilisco il valore di 'longest'
+//if (firstWord.length > secondWord.length) {
+  
+  //  longest= firstWord
+
+//} else {
+
+  //  longest = secondWord
+
+//}
+ ////stampo longest
+//alert(longest)
 
 
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ SNACK
 1) Chiedi un numero all'utente tramite prompt. Finchè il numero inserito non è 42, continua a chiedere il numero. Se il numero è 42 congratulati.
 
 2) Il software deve chiedere per 10 volte all’utente di inserire un numero. Poi stampa la somma di tutti i numeri inseriti.
+
+3) L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
